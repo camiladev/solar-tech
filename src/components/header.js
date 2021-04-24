f6bba79a2db1556db2c2d03a7b1ad37bd0c1317f
Camila Matos <camila.mila148@gmail.com>
2021-04-24 17:51:23 -0300

    Iniciado projeto e criado rotas e componente header

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+
+
+export default function Header(){
+
+    return(
+        <header>
+            <img src="/assets/logo.svg" alt="Logo X Solar Tech"/>
+
+            <span>Sair</span>
+        </header>
+    )
+}
