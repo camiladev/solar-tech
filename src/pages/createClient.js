b82e1715a2fdf5ee4613e07ef4ab76ecb5e15a1c
Camila Matos <camila.mila148@gmail.com>
2021-04-26 16:51:03 -0300

    Ajustado transbordo do form

--- src/pages/createClient.js
+++ src/pages/createClient.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import Layout from "../components/layout";
+
+import { FaArrowLeft, FaPlusSquare } from "react-icons/fa";
 
 import '../styles/form.css'
 
 export default function CreateClient(){
+    const [addClient, setAddClient] = useState([])
+    const [addAddress, setAddAddress] = useState([""])
+
+    function addInputAddress(){
+        
+        setAddAddress([...addAddress,""])
+    }
 
     return(
         <Layout>
             <div className="formContainer">
                 <header>
-                    <span>X</span>
+                    <span><FaArrowLeft /></span>
                     <h1>Adicionar Cliente</h1>                    
                 </header>
 
@@ -56,95 +66,102 @@ export default function CreateClient(){
                         <div className="addressForm">
                             <div className="header">
                                 <h3>Endereço</h3>
-                                <span>+</span>
+                                
+                                <span onClick={()=> addInputAddress()}><FaPlusSquare /></span>
+                                
                             </div>
-                            <div className="dataAddress">
-                                <p className='radio'>
-                                    <label htmlFor="">Endereço Principal?</label>
-                                    <p>
-                                        <input 
-                                            type="radio"
-                                            name='principal'
-                                            value='sim'  
-                                            required 
-                                        />
-                                        <label htmlFor="">Sim</label>
+                            { addAddress.map((address, index) => {
+                                return(
 
-                                        <input 
-                                            type="radio"
-                                            name='principal'
-                                            value='nao'  
-                                            required 
-                                        />
-                                        <label htmlFor="">Não</label>
+                                    <div className="dataAddress">
+                                        <p className='radio'>
+                                            <label htmlFor="">Endereço Principal?</label>
+                                            <p>
+                                                <input 
+                                                    type="radio"
+                                                    name='principal'
+                                                    value='sim'  
+                                                    required 
+                                                />
+                                                <label htmlFor="">Sim</label>
 
-                                    </p>
-                                </p>
-                                <p className='select'>
-                                    <label htmlFor="">Tipo</label>
-                                    <select name="selectTipo" id="type">
-                                        <option value="residencial">Residencial</option>
-                                        <option value="comercial">Comercial</option>
-                                        <option value="rural">Rural</option>
-                                        <option value="casaDePraia">Casa de praia</option>
-                                    </select>
-                                </p>
-                                <p>
-                                    <label htmlFor="">Rua</label>
-                                    <input 
-                                        type="text"
-                                        name='rua'
-                                        placeholder='Nome da rua'   
-                                        required 
-                                    />
-                                </p>
-                                <p>
-                                    <label htmlFor="">Nº</label>
-                                    <input 
-                                        type="text"
-                                        name='num'
-                                        placeholder='Numero'   
-                                        required 
-                                    />
-                                </p>
-                                <p>
-                                    <label htmlFor="">Complemento</label>
-                                    <input 
-                                        type="text"
-                                        name='comp' 
-                                        required 
-                                    />
-                                </p>
-                                <p>
-                                    <label htmlFor="">Bairro</label>
-                                    <input 
-                                        type="text"
-                                        name='bairro'
-                                        placeholder='Bairro'   
-                                        required 
-                                    />
-                                </p>
+                                                <input 
+                                                    type="radio"
+                                                    name='principal'
+                                                    value='nao'  
+                                                    required 
+                                                />
+                                                <label htmlFor="">Não</label>
 
-                                <p>
-                                    <label htmlFor="">Cidade</label>
-                                    <input 
-                                        type="text"
-                                        name='cidade'
-                                        placeholder='Cidade'   
-                                        required 
-                                    />
-                                </p>
-                                <p>
-                                    <label htmlFor="">UF</label>
-                                    <input 
-                                        type="text"
-                                        name='uf'
-                                        placeholder='UF'   
-                                        required 
-                                    />
-                                </p>
+                                            </p>
+                                        </p>
+                                        <p className='select'>
+                                            <label htmlFor="">Tipo</label>
+                                            <select name="selectTipo" id="type">
+                                                <option value="residencial">Residencial</option>
+                                                <option value="comercial">Comercial</option>
+                                                <option value="rural">Rural</option>
+                                                <option value="casaDePraia">Casa de praia</option>
+                                            </select>
+                                        </p>
+                                        <p>
+                                            <label htmlFor="">Rua</label>
+                                            <input 
+                                                type="text"
+                                                name='rua'
+                                                placeholder='Nome da rua'   
+                                                required 
+                                            />
+                                        </p>
+                                        <p>
+                                            <label htmlFor="">Nº</label>
+                                            <input 
+                                                type="text"
+                                                name='num'
+                                                placeholder='Numero'   
+                                                required 
+                                            />
+                                        </p>
+                                        <p>
+                                            <label htmlFor="">Complemento</label>
+                                            <input 
+                                                type="text"
+                                                name='comp' 
+                                                required 
+                                            />
+                                        </p>
+                                        <p>
+                                            <label htmlFor="">Bairro</label>
+                                            <input 
+                                                type="text"
+                                                name='bairro'
+                                                placeholder='Bairro'   
+                                                required 
+                                            />
+                                        </p>
 
-                            </div>
+                                        <p>
+                                            <label htmlFor="">Cidade</label>
+                                            <input 
+                                                type="text"
+                                                name='cidade'
+                                                placeholder='Cidade'   
+                                                required 
+                                            />
+                                        </p>
+                                        <p>
+                                            <label htmlFor="">UF</label>
+                                            <input 
+                                                type="text"
+                                                name='uf'
+                                                placeholder='UF'   
+                                                required 
+                                            />
+                                        </p>
+
+                                    </div>
+                                )
+                            })}
                         </div>
                         <div className="button">
                             <button type='submit'>Salvar</button>
